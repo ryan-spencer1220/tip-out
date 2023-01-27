52810873bbcdd0a7cc7a5b702db9c978181b10a7
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-01-26 18:58:10 -0800

    style auth page

--- App.js
+++ App.js
@@ -5,12 +5,3 @@ import Auth from "./src/screens/Auth";
 export default function App() {
   return <Auth />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
