82b854671efae42cf387aa5716d3fbe1ec3f758f
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-02-25 15:16:56 -0800

    style and add functionality to settings rows

--- src/screens/Settings.jsx
+++ src/screens/Settings.jsx
@@ -18,7 +18,11 @@ import { Entypo } from "@expo/vector-icons";
 const Settings = ({ navigation }) => {
   const [darkMode, setDarkMode] = useState(true);
   const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [cashSetting, setCashSetting] = useState(true);
+  const [hourlySetting, setHourlySetting] = useState(true);
+  const [salesSetting, setSalesSetting] = useState(true);
+  const [coversSetting, setCoversSetting] = useState(true);
+
   return (
     <SafeAreaView style={styles.background}>
       <ScrollView contentContainerStyle={styles.content}>
@@ -64,18 +68,58 @@ const Settings = ({ navigation }) => {
           <TouchableOpacity style={styles.settingsRow}>
             <Text style={styles.smallText}>Cash & Credit Tips</Text>
             <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+              style={{ marginLeft: "auto", marginRight: 16 }}
+              trackColor={{ false: "#2b825b", true: "#3e3e3e" }}
+              thumbColor={cashSetting ? "#2b825b" : "#f4f3f4"}
               ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleSwitch}
-              value={isEnabled}
+              onValueChange={() => setCashSetting(!cashSetting)}
+              value={cashSetting}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.settingsRow}></TouchableOpacity>
-          <TouchableOpacity style={styles.settingsRow}></TouchableOpacity>
-          <TouchableOpacity style={styles.settingsRow}></TouchableOpacity>
+          <TouchableOpacity style={styles.settingsRow}>
+            <Text style={styles.smallText}>Hourly Wage</Text>
+            <Switch
+              style={{ marginLeft: "auto", marginRight: 16 }}
+              trackColor={{ false: "#2b825b", true: "#3e3e3e" }}
+              thumbColor={hourlySetting ? "#2b825b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => setHourlySetting(!hourlySetting)}
+              value={hourlySetting}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.settingsRow}>
+            <Text style={styles.smallText}>Total Sales</Text>
+            <Switch
+              style={{ marginLeft: "auto", marginRight: 16 }}
+              trackColor={{ false: "#2b825b", true: "#3e3e3e" }}
+              thumbColor={salesSetting ? "#2b825b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => setSalesSetting(!salesSetting)}
+              value={salesSetting}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.settingsRow}>
+            <Text style={styles.smallText}>Covers/Guests</Text>
+            <Switch
+              style={{ marginLeft: "auto", marginRight: 16 }}
+              trackColor={{ false: "#2b825b", true: "#3e3e3e" }}
+              thumbColor={coversSetting ? "#2b825b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => setCoversSetting(!coversSetting)}
+              value={coversSetting}
+            />
+          </TouchableOpacity>
           <Text style={styles.largeText}>App Settings</Text>
+          <TouchableOpacity style={styles.settingsRow}>
+            <Text style={styles.smallText}>My Jobs</Text>
+          </TouchableOpacity>
           <Text style={styles.largeText}>Terms & Agreements</Text>
+          <TouchableOpacity style={styles.settingsRow}>
+            <Text style={styles.smallText}>Terms of Use</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.settingsRow}>
+            <Text style={styles.smallText}>Privacy Policy</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -148,6 +192,7 @@ const styles = StyleSheet.create({
   smallText: {
     color: "gray",
     fontFamily: "Inter_800ExtraBold",
+    marginStart: 16,
   },
   textDark: {
     color: "#707070",
@@ -155,17 +200,6 @@ const styles = StyleSheet.create({
   headline: {
     fontSize: 80,
     color: "#EDEDED",
-  },
-  line: {
-    borderBottomColor: "white",
-    borderBottomWidth: 2,
-    margin: 10,
-  },
-  or: {
-    color: "white",
-    paddingTop: 10,
-    paddingBottom: 10,
-    textAlign: "center",
   },
   item: {
     height: 40,
